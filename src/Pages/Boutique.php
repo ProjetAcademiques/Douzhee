c0880a9ac5062f70f886fadcdb56f09389e70ed7
Nagisannnn <146878751+Nagisannnn@users.noreply.github.com>
2025-01-06 08:42:05 +0100

    nik

--- src/Pages/Boutique.php
+++ src/Pages/Boutique.php
@@ -1,31 +1,38 @@
 <?php
     require_once("../Utils/headerInit.php");
+    require_once("../CRUD/CRUDSkinAchetable.php");
 ?>
     <link rel="stylesheet" href="../../assets/CSS/Theme.css">
     <link rel="stylesheet" href="../../assets/CSS/styleBoutique.css">
     <link rel="stylesheet" href="../../assets/CSS/styleHeader.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 <body>
     <?php require_once("../Utils/headerBody.php"); ?>
     <div id="Boutique">
         
-        <button class="nav" id="navUp">Up</button>
+        <button class="nav" id="navUp">
+            <i class="fa-solid fa-chevron-up"></i>
+        </button>
 
         <div class="container">
             <div id="Theme" class="store">
                 <h2>Thèmes</h2>
+                <div class="sold">
+                </div>
             </div>
 
             <div id="Musique" class="store">
                 <h2>Musiques</h2>
-            </div>
+                <div class="sold">
 
-            <div id="Dés" class="store">
-                <h2>Skins de Dés</h2>
+                </div>
             </div>
         </div>
         
-        <button class="nav" id="navDown">Down</button>
+        <button class="nav" id="navDown">
+            <i class="fa-solid fa-chevron-down"></i>
+        </button>
     </div>
 
     <script src="../../assets/JS/updateBoutique.js"></script>

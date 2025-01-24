9bd587f9ab1af53788a1840643697d1ba4cbca33
Nagisannnn <146878751+Nagisannnn@users.noreply.github.com>
2025-01-24 08:27:01 +0100

    save

--- src/Pages/Classement.php
+++ src/Pages/Classement.php
@@ -3,51 +3,49 @@
     require_once("../CRUD/CRUDJoueur.php");
     require_once("../Utils/headerInit.php");
     require_once("../CRUD/CRUDClassement.php");
-    require_once("../Utils/headerBody.php");
 
     if (!isset($_SESSION['userId'])){
         require_once("../Utils/redirection.php");
     }
 ?>
-
-    <link rel="stylesheet" href="../../assets/css/styleHeader.css"> 
-    <link rel="stylesheet" href="../../assets/css/styleGlobal.css">     
-    <link rel="stylesheet" href="../../assets/css/Classement.css">
+    <link rel="stylesheet" href="../../assets/CSS/Theme.css">
+    <link rel="stylesheet" href="../../assets/CSS/styleHeader.css">     
+    <link rel="stylesheet" href="../../assets/CSS/Classement.css">
 </head>
 
 <html>
-    <body>
-        <div id = "ldFlexContainer">
-        <table id = "leaderBoard">
-            <tbody>
-                <tr>
-                    <th></th>
-                    <th>Joueur</th>
-                    <th id = "leaderBoardMode"></th>
-                </tr>
-                <tr>
-                    <td>#1</td>
-                    <td id = "nom1">--</td>
-                    <td id = "stat1">--</td>
-                </tr>
-                <tr>
-                    <td>#2</td>
-                    <td id = "nom2">--</td>
-                    <td id = "stat2">--</td>
-                </tr>
-                <tr>
-                    <td>#3</td>
-                    <td id = "nom3">--</td>
-                    <td id = "stat3">--</td>
-                </tr>
-            </tbody>
-        </table>
+<body>
+    <?php require_once("../Utils/headerBody.php"); ?>
+    <table id = "leaderBoard">
+        <tbody>
+            <tr>
+                <th></th>
+                <th>Joueur</th>
+                <th id = "leaderBoardMode"></th>
+            </tr>
+            <tr>
+                <td>#1</td>
+                <td id = "nom1">--</td>
+                <td id = "stat1">--</td>
+            </tr>
+            <tr>
+                <td>#2</td>
+                <td id = "nom2">--</td>
+                <td id = "stat2">--</td>
+            </tr>
+            <tr>
+                <td>#3</td>
+                <td id = "nom3">--</td>
+                <td id = "stat3">--</td>
+            </tr>
+        </tbody>
+    </table>
 
-
+    <div>
         <button type = "button" id = "leaderBoardScore">Leaderboard par score</button>
         <button type = "button" id = "leaderBoardDouzhee">Leaderboard par monnaie</button>
         <button type = "button" id = "leaderBoardSucces">Leaderboard par succes</button>
-
-        <script src = "../../assets/JS/scriptClassement.js" type = "text/javascript"></script>
-    </body>
+    </div>
+    <script src = "../../assets/JS/scriptClassement.js" type = "text/javascript"></script>
+</body>
 </html>
